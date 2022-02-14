2d16c6d33a971ea034fd8d4fd082efd499a3640e
Yury Durov <43110604+some-crap@users.noreply.github.com>
2022-02-14 20:32:02 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+// config.php
+//
+//
+//
+// Код доступен по лицензии
+// Creative Commons «Attribution-NonCommercial» INTERNATIONAL
+// https://creativecommons.org/licenses/by-nc/4.0/
+//
+//
+//
+define ("VK_TOKEN", '');
+define ("VK_API_VERSION", '5.131');
+define ('DB_PASS', '');
+define ('DB_NAME', '');
+define ('DB_USERNAME', '');
+define ('DB_HOST', 'localhost:3306');
+define ('VK_SECRET', '');
+define ('VK_GROUP_ID', 1234567890);
+define ('VK_VERIFICATION', "");
